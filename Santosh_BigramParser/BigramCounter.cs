a205276190d2c996455fd2234fd0bd3f1aee6eff
Santosh Sekhar Muchkur Bogarajula <santosh@SANTOSHs-MacBook-Pro-2.local>
2019-11-12 14:34:46 -0600

    initial commit

---
+++ Santosh_BigramParser/BigramCounter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Santosh_BigramParser
+{
+    public class BigramCounter
+    {
+        public Dictionary<string, int> InputFile(string path)
+        {
+            var bucket = new Dictionary<string, int>();
+            if (!File.Exists(path)) return bucket;
+
+            using (var doc = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (var buffer = new BufferedStream(doc))
+            using (var stream = new StreamReader(buffer))
+            {
+                string input;
+                while ((input = stream.ReadLine()) != null)
+                {
+                    var after = RemoveSymbols(input);
+                    var words = after.ToLower().Split(' ');
+                    for (var i = 0; i < words.Length - 1; i++)
+                    {
+                        var sentence = $"{words[i]} {words[i + 1]}";
+                        var term = bucket.FirstOrDefault(r => r.Key == sentence);
+                        if (term.Value > 0)
+                        {
+                            bucket[sentence] = term.Value + 1;
+                        }
+                        else
+                        {
+                            bucket.Add(sentence, 1);
+                        }
+                    }
+                }
+            }
+            return bucket;
+        }
+
+        private static string RemoveSymbols(string input)
+        {
+            return input.Replace(".", "").Replace("!", "").Replace("?", "").Replace("\"", "").Replace(",", "").Replace("#", "").Replace(":", "")
+                .Replace(";", "").Replace("%", "").Replace("^", "").Replace("*", "").Replace("(", "").Replace(")", "").Replace("[", "").Replace("]", "")
+                .Replace("/", "").Replace("~", "").Replace("|", "").Replace("<", "").Replace(">", "").Replace("@", "").Replace("$", "").Replace("&", "").Replace("`", "");
+        }
+    }
+}
---
+++ Santosh_BigramParser/Program.cs
@@ -0,0 +1,43 @@
+﻿using Santosh_BigramParser;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Santosh_Bigramparser
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Any())
+            {
+                var filePath = args[0];
+                if (File.Exists(filePath))
+                {
+                    var values = new BigramCounter();
+                    try
+                    {
+                        var results = values.InputFile(filePath);
+                        foreach (var result in results)
+                        {
+                            Console.WriteLine($" '{result.Key}' \t {result.Value}");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error: {ex.Message}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Error: Could not find file at {filePath}");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+
